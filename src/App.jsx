9a842148b6301ac0e73d0a82cfb8144efc100e40
Ifra Razzaq <126340268+IfraRazzaq@users.noreply.github.com>
2023-10-08 08:06:04 +0000

    homepage

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Ifra Razzaq",
+  title: "Flutter Developer",
+  email: "ifrahrazzaq31@gmail.com",
+  gitHub: "IfraRazzaq",
+  instagram: "ifra_razzaq1",
+  linkedIn: "ifra-razzaq-690aba214",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";

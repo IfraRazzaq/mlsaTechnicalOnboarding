ebf7cc90f5ae2172640913553e151e64e45ffe0c
Ifra Razzaq <126340268+IfraRazzaq@users.noreply.github.com>
2023-10-08 08:11:44 +0000

    picture commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "https://avatars.githubusercontent.com/u/126340268?v=4";
+import image from "/workspaces/mlsaTechnicalOnboarding/__images__/126340268.jpeg";
 
 const imageAltText = "Ifra Razzaq";
 
